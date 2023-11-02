38e44980d1a64797b3e19a32b0f1d4222191c7a7
79234 <marusya0323@gmail.com>
2023-11-02 18:33:54 +0700

    Регистрация моделей

--- designpro/catalog/admin.py
+++ designpro/catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MainConfig, AdvUser, Request
 
 # Register your models here.
+admin.site.register(MainConfig)
+admin.site.register(AdvUser)
+admin.site.register(Request)
